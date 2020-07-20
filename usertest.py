f0fa025b875e8ae8191623c8552e63f0eb8d601d
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-21 00:10:42 +0300

    add test init to test if the object is initialized properly

--- usertest.py
+++ usertest.py
@@ -14,3 +14,12 @@ class TestUser(unittest.TestCase):
         tearDown method that does clean up after each test case has run.
         """
         User.users_array = []
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.first_name, "James")
+        self.assertEqual(self.new_user.last_name, "Muriuki")
+        self.assertEqual(self.new_user.phone_number, "0712345678")
+        self.assertEqual(self.new_user.email, "james@ms.com")    
