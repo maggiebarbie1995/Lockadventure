e26554d5207021ad95e917f9fb5275c2b404f1fc
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-21 09:03:44 +0300

    add method that saves credential objects into cred arrays

--- credential.py
+++ credential.py
@@ -16,8 +16,14 @@ def check_user(cls,first_name,password):
             current_user = user.first_name
             return current_user
 
- def __init__(self, user_name, password, email):
+def __init__(self, user_name, password, email):
         self.user_name = user_name
         self.password = password
         self.email = email
+
+def save_credential(self):
+        """
+        save_contact method saves credentials objects into credential_array
+        """
+        Credential.credential_array.append(self)       
 
